1c6d37e8a616116bc1553ef3630684f5469cb4b7
raven-south4 <rava.flores.au@phinmaed.com>
2026-09-02 02:10:28 +0000

    p1

---
+++ sum_of_three_numbers.py
@@ -0,0 +1,9 @@
+num1 = input("Enter first number: ")
+
+num2 = input("Enter first number: ")
+
+num3 = input("Enter first number: ")
+
+total = int(num1) + int(num2) + int(num3)
+
+print("The sum is: ", total)
